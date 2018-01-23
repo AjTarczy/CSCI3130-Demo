e8f6bc0ea05f9198bceceda6e78442167d257ef4
AjTarczy <ajtarczy@dal.ca>
2018-01-23 14:53:25 -0400

    Added a EditText, as well as a button with the ability to copy the text from the EditText, into a seperate TextView on click.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.view.View;
+import java.lang.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonClick(View v) {
+        EditText editBox = (EditText)findViewById(R.id.copyBox); //sets the EditText on screen to an object
+        TextView pasteBox = (TextView)findViewById(R.id.pasteBox); //sets the TextView on screen to an object
+        String enteredText = editBox.getText().toString(); //stores the current value of EditText into a variable
+        pasteBox.setText(enteredText); //pastes the string entered into the EditText, into the TextView.
+
+    }
+
 }
